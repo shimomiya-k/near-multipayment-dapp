7610f096f72f3c666b0e13c5868225a041815b0c
Kazuma Shimomiya <shimomiya_@indent.co.jp>
2022-09-17 21:24:25 +0900

    section-2-lesson2

---
+++ frontend/lib/view/screens/send.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class Send extends StatefulWidget {
+  const Send({Key? key}) : super(key: key);
+
+  @override
+  State<Send> createState() => _HomeState();
+}
+
+class _HomeState extends State<Send> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Send Screen")),
+    );
+  }
+}
---
+++ frontend/lib/view/screens/wallet.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class Wallet extends StatefulWidget {
+  const Wallet({Key? key}) : super(key: key);
+
+  @override
+  State<Wallet> createState() => _HomeState();
+}
+
+class _HomeState extends State<Wallet> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Wallet Screen")),
+    );
+  }
+}
